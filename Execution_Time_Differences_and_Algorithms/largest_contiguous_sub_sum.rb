21de23bc2bb1a55c724ddaae728e8d7094d5838f
Roderick Mendoza <rodmendoza07@gmail.com>
2023-02-24 13:43:32 -0800

    complete Execution...folder

--- Execution_Time_Differences_and_Algorithms/largest_contiguous_sub_sum.rb
+++ Execution_Time_Differences_and_Algorithms/largest_contiguous_sub_sum.rb
@@ -4,7 +4,7 @@
 #     sums_arr = []
 #     arr.each_with_index do |el, i|
 #          arr.each_with_index do |el2, j|
-#             if i <= j 
+#             if i <= j
 #                 sums_arr << arr[i..j]
 #             end
 #          end
@@ -15,20 +15,29 @@
 #     sums_arr.sort[-1]
 # end
 
+# phase 2
 
-
+def largest_contiguous_subsum(arr)
+    largest_sum = arr[0]
+    current_sum = []
+    arr.each_with_index do |num, i|
+        current_sum[i] = current_sum[i - 1] ? [num, num + current_sum[i - 1]].max : num
+        largest_sum = [largest_sum, current_sum[i]].max
+    end
+    largest_sum
+end
 
 
 list = [5, 3, -7]
 p largest_contiguous_subsum(list) # => 8
 
-# # possible sub-sums
-# [5]           # => 5
-# [5, 3]        # => 8 --> we want this one
-# [5, 3, -7]    # => 1
-# [3]           # => 3
-# [3, -7]       # => -4
-# [-7]          # => -7
+# possible sub-sums
+[5]           # => 5
+[5, 3]        # => 8 --> we want this one
+[5, 3, -7]    # => 1
+[3]           # => 3
+[3, -7]       # => -4
+[-7]          # => -7
 
 list = [2, 3, -6, 7, -6, 7]
 p largest_contiguous_subsum(list) # => 8 (from [7, -6, 7])
