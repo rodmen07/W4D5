664755169012e1d50eed0ccc818bacfe87aa7b4d
Roderick Mendoza <rodmendoza07@gmail.com>
2023-02-24 16:27:16 -0800

    windowed_max_range Phase III/IV

--- Windowed_Max_Range/windowed_max_range.rb
+++ Windowed_Max_Range/windowed_max_range.rb
@@ -9,7 +9,7 @@ def windowed_max_range(array, window_size)
             # debugger
             result = max - min
            if result > current_max_range
-             current_max_range = result 
+             current_max_range = result
            end
        end
     end
@@ -22,29 +22,81 @@ p windowed_max_range([1, 0, 2, 5, 4, 8], 4) # == 6 # 2, 5, 4, 8
 p windowed_max_range([1, 3, 2, 5, 4, 8], 5) # == 6 # 3, 2, 5, 4, 8
 
 class MyQueue
+  attr_reader :store
 
   def initialize
     @store = []
   end
 
-  def enqueue
-
+  def enqueue(ele)
+    self.store.unshift(ele)
   end
 
   def dequeue
-
+    self.store.pop
   end
 
   def peek
-
+    self.store[-1]
   end
 
-  def window_size
+  def size
+    self.store.length
+  end
+
+  def empty?
+    self.store.empty?
+  end
+end
+
+class MyStack
+  attr_reader :store
+
+  def initialize
+    @store = []
+  end
+
+  def peek
+    self.store[-1]
+  end
+
+  def size
+    self.store.length
+  end
+
+  def empty?
+    self.store.empty?
+  end
+
+  def pop
+    self.store.pop
+  end
+
+  def push
+    self.store.push
+  end
+
+end
+
+class StackQueue
+  attr_reader :stack_1, :stack_2
+
+  def initialize
+    @stack_1 = MyStack.new
+    @stack_2 = MyStack.new
+  end
+
+  def size
     
   end
 
   def empty?
+  end
 
+  def enqueue(ele)
+  end
+
+  def dequeue
   end
 
 end
